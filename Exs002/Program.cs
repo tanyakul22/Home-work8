42954d16b1a7b1328e6cb70482788e9360d2ab6c
Tatyana Kulakova <117149308+tanyakul22@users.noreply.github.com>
2022-11-16 22:07:53 +1000

    Dz8

---
+++ Exs002/Program.cs
@@ -0,0 +1,25 @@
+﻿const int a=3,b=4;
+int [,]array = new int[a,b];
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+           Console.Write( array[i,j]);
+        }
+        Console.WriteLine();
+    }
+    int sum=0,c=0;
+    for(int j=0;j<array.GetLength(1);j++)
+        sum+=array[0,j];
+    for(int i=1;i<array.GetLength(0);i++)
+    {
+      int tsum=0;
+      for (int j=0;j<array.GetLength(1);j++)
+         tsum+=array[i,j];
+      if (tsum<sum)
+      {
+        sum=tsum;
+        c=i;}
+    }
+    Console.WriteLine("строка:"+ c + " является строкой с наименьшей суммой элементов. Сумма составляет:" + sum);
---
+++ Exs004/Program.cs
@@ -0,0 +1,33 @@
+﻿int [, ,] array=new int[2,2,2];
+ List<int> nums= new List<int>();
+ for (int i=0;i<90;i++)
+ nums.Add(i+10);
+ for (int i=0;i<array.GetLength(0);i++)
+ {
+
+    for (int j=0;j<array.GetLength(1);j++)
+    {
+        for (int k=0;k<array.GetLength(2);k++)
+        {
+            if(nums.Count==0)
+            array[i,j,k]=0;
+            else
+            {
+               int temp=new Random().Next(nums.Count);
+               array[i,j,k]=nums[temp];
+               nums.RemoveAt(temp);
+            }
+        }
+    }
+ }
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k ++)
+            {
+                Console.Write($"{array[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+    }
